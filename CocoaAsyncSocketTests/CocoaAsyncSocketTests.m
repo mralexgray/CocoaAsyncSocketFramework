ee0b1e6af3c6e1cc9131b4efaf4a0ef8e89801e8
Arlo O'Keeffe <mailtoarlo@gmail.com>
2012-10-26 10:25:06 -0600

    Reverted changes to CocoaAsyncSocket, updated build settings

---
+++ CocoaAsyncSocketTests/CocoaAsyncSocketTests.m
@@ -0,0 +1,31 @@
+#import "CocoaAsyncSocketTests.h"
+
+#import <CocoaAsyncSocket/GCDAsyncSocket.h>
+#import <CocoaAsyncSocket/GCDAsyncUdpSocket.h>
+#import <CocoaAsyncSocket/AsyncSocket.h>
+#import <CocoaAsyncSocket/AsyncUdpSocket.h>
+
+@implementation CocoaAsyncSocketTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+	dispatch_queue_t mainQueue = dispatch_get_main_queue();
+	
+	[[GCDAsyncSocket alloc] initWithDelegate:self delegateQueue:mainQueue];
+}
+
+@end
